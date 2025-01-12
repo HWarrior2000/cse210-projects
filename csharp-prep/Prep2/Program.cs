dd641d57d085aaf780741baa45334d775ffdca38
Helaman Tillotson <helaman.tillotson@gmail.com>
2025-01-11 23:49:35 -0700

    prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string response = Console.ReadLine();
+        int grade = int.Parse(response);
+
+        string leterGrade = "";
+
+        if (grade >= 90)
+        {
+            leterGrade = "A";
+        }
+
+        else if (grade >= 80 && grade < 90)
+        {
+            leterGrade = "B";
+        }
+
+        else if (grade >= 70 && grade < 80)
+        {
+            leterGrade = "C";
+        }
+
+        else if (grade >= 60 && grade < 70)
+        {
+            leterGrade = "D";
+        }
+
+        else
+        {
+            leterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {leterGrade}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("You failed!");
+        }
     }
 }
