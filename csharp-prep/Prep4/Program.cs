58bff78a2768b0fed540957ac18a47caffb63dd2
Helaman Tillotson <helaman.tillotson@gmail.com>
2025-01-16 14:11:58 -0700

    prep 4 completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userNumber = 0;
+        do
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if(userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        } while(userNumber != 0);
+
+        int total = 0;
+        int largest = 0;
+        foreach (int number in numbers)
+        {
+            if(number > largest)
+            {
+                largest = number;
+            }
+            total += number;
+        }
+
+        Console.WriteLine($"The sum is: {total}");
+
+        float average = ((float)total) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
