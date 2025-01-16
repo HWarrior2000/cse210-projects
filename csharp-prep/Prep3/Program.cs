d6d45259d3d7ae58fe3d013e91a1f8de5ac15d2f
Helaman Tillotson <helaman.tillotson@gmail.com>
2025-01-16 13:31:56 -0700

    practice 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int userNumber = 0;
+        do
+        {
+            Console.Write("What is your guess: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if(userNumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if(userNumber < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (userNumber != number);
     }
 }
