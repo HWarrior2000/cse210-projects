bd6eded6c835e5b557fae96e2e39de793503aa0e
Helaman Tillotson <helaman.tillotson@gmail.com>
2025-01-21 19:12:44 -0700

    practice with classes/Learing02

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Movie favoriteMovie = new Movie();
+        favoriteMovie._title = "Star Wars";
+        favoriteMovie._year = 1977;
+        favoriteMovie._runtime = 150;
+        favoriteMovie._rating = "PG";
+
+        Movie otherMovie = new Movie();
+        otherMovie._title = "Avatar";
+        otherMovie._year = 2009;
+        otherMovie._rating = "PG-13";
+        otherMovie._runtime = 162;
+        
+        favoriteMovie.Display();
+        otherMovie.Display();
+
+        DisplayMovie(favoriteMovie);
+        DisplayMovie(otherMovie);
+    }
+
+    static void DisplayMovie(Movie aMovie)
+    {
+        Console.WriteLine($"{aMovie._title} - {aMovie._year}");
     }
 }
