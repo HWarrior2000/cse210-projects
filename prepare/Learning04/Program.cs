4aa0d1c4dcdb1c46588beebde87ca2637d5a8f36
Helaman Tillotson <helaman.tillotson@gmail.com>
2025-02-20 15:41:48 -0700

    Learning 4 complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment num1 = new Assignment("Helaman Tillotson", "Algebra");
+        Console.WriteLine(num1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment math1 = new MathAssignment("Joshua", "Calculus", "7.1", "1-10");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writing1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
+        Console.WriteLine();
     }
 }
