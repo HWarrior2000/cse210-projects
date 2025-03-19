1a2941f9835f593b96c66b5f9b52dba0a7bf1f41
Helaman Tillotson <helaman.tillotson@gmail.com>
2025-03-18 22:38:09 -0600

    prep complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Rectangle rec1 = new Rectangle(5, 4, "gray");
+        Rectangle rec2 = new Rectangle(7, 2, "pink");
+        Square squ1 = new Square(6, "black");
+        Square squ2 = new Square(10, "green");
+        Circle cir1 = new Circle(11, "purple");
+        Circle cir2 = new Circle(4, "yellow");
+        shapes.Add(rec1);
+        shapes.Add(rec2);
+        shapes.Add(squ1);
+        shapes.Add(squ2);
+        shapes.Add(cir1);
+        shapes.Add(cir2);
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"Area: {shape.GetArea()}\nColor: {shape.GetColor()}\n");
+        }
     }
 }
